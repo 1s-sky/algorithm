a7d97d3812eeb4d517706c525d159d4af2611154
1s-sky <bivoaov5@gmail.com>
2023-09-21 12:32:35 +0900

    8.문장 회문 검사

--- String/PalindromeSentence.java
+++ String/PalindromeSentence.java
@@ -17,20 +17,14 @@ import java.util.Scanner;
 
 public class PalindromeSentence {
     public static String solution(String str) {
-        String answer;
-        StringBuilder alpbet = new StringBuilder();
-        char[] cAry = str.toCharArray();
+        String answer = "NO";
         
-        //알파벳만 걸러내기
-        for(char c : cAry){
-            if(Character.isAlphabetic(c)){
-                alpbet.append(Character.toLowerCase(c));
-            }
-        }
+        //알파벳만 걸러내기 => replaceAll을 이용하여 정규식 사용
+        str = str.toLowerCase().replaceAll("[^a-z]", "");
+        String reverse = new StringBuilder(str).reverse().toString();
 
         //회문검사
-        if((alpbet.toString() + alpbet.reverse().toString()).equals((alpbet.reverse().toString() + alpbet.toString()))) answer = "YES";
-        else answer = "NO";
+        if(str.equals(reverse)) answer = "YES";
         return answer;
     }
     public static void main(String[] args) {
