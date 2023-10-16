beef2148a8a9e085b23925aa0fa69632a708b0f4
1s-sky <bivoaov5@gmail.com>
2023-10-16 16:25:52 +0900

    8.등수 구하기

---
+++ Array/Ranking.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Ranking {
+    public String solution(int[] ary, int n) {
+        StringBuilder answer = new StringBuilder();
+        for (int i = 0; i < n; i++) {
+            int count = 1;
+            for (int j = 0; j < n; j++) {
+                if (ary[i] < ary[j])
+                    count++;
+            }
+            answer.append(count).append(' ');
+        }
+        return answer.toString();
+    }
+
+    public static void main(String[] args) {
+        Ranking r = new Ranking();
+        Scanner kb = new Scanner(System.in);
+        int n = kb.nextInt();
+        int[] ary = new int[n];
+        for (int i = 0; i < n; i++) {
+            ary[i] = kb.nextInt();
+        }
+        System.out.println(r.solution(ary, n));
+        kb.close();
+    }
+}
