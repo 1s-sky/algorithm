f0916f649858ee7b7835cd87beddd62c74039f3a
1s-sky <bivoaov5@gmail.com>
2024-03-19 16:50:34 +0900

    17.연속된 자연수의 합

--- Array/SumSuccessiveNum.java
+++ Array/SumSuccessiveNum.java
@@ -4,7 +4,7 @@ public class SumSuccessiveNum {
     public static int solution(int n){
         int answer = 0;
         int temp = 0;
-        int p1=0, p2=0;
+        // int p1=0, p2=0;
         for(int i=0; i<n/2+1; n++){
             if(temp == n) { answer++; }
         }
