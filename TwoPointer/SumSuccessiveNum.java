bfe5446b2a3ed03f84f4994395e994957bcd9424
1s-sky <bivoaov5@gmail.com>
2024-06-23 20:23:23 +0900

    5.연속된 자연수의 합

--- TwoPointer/SumSuccessiveNum.java
+++ TwoPointer/SumSuccessiveNum.java
@@ -3,11 +3,30 @@ import java.util.Scanner;
 public class SumSuccessiveNum {
     public static int solution(int n){
         int answer = 0;
-        int temp = 0;
-        // int p1=0, p2=0;
-        for(int i=0; i<n/2+1; n++){
-            if(temp == n) { answer++; }
+        int sum = 0, lp = 0;
+        int[] arr = new int[n/2+1];
+        for(int i=0; i<arr.length; i++) arr[i] = i+1; 
+        for(int rp=0; rp<arr.length; rp++){
+            sum += arr[rp];
+
+            while(sum > n) { sum -= arr[lp]; lp++;} 
+            if(sum == n) answer++;
         }
+
+
+
+
+
+        //수학 공식
+        // int cnt = 1;
+
+        // n--;
+        // while(n>0){
+        //     cnt++;
+        //     n -= cnt;
+        //     if(n%cnt==0) answer++;
+        // }
+
         return answer;
     }
     public static void main(String[] args) {
