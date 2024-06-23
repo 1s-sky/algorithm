ab0c1a870914606a52c740e4217502c7a51a6601
1s-sky <bivoaov5@gmail.com>
2024-06-23 19:25:13 +0900

    6.최대 길이 연속부분수열

--- TwoPointer/ContinuousSeqMax.java
+++ TwoPointer/ContinuousSeqMax.java
@@ -7,6 +7,7 @@ public class ContinuousSeqMax {
         ArrayList<Integer> arr0 = new ArrayList<>();
         int answer = 0;
         int temp;
+
         for (int i = 0; i<arr.length; i++) {
             if(arr[i] == 0) arr0.add(i);
         }
@@ -15,17 +16,26 @@ public class ContinuousSeqMax {
         answer = arr0.get(k);
 
         for(int i = 0; i < arr0.size() - (k+1); i++){
-
             //중간 부분 길이 계산하기
-            temp = arr0.get(i + k + 1) - arr0.get(i) - 1;
-            //길이가 더 길면 answer 변경
-            if (temp > answer) answer = temp;
+            answer = Math.max(answer, arr0.get(i + k + 1) - arr0.get(i) - 1);
         }
 
         //끝부분 길이
-        temp = arr.length - arr0.get(arr0.size() - k - 1) - 1;
-        //길이가 더 길면 answer 변경
-        if (temp > answer) answer = temp;
+        answer = Math.max(answer, arr.length - arr0.get(arr0.size() - k - 1) - 1);
+
+        //Two Pointer
+        // int lp = 0;
+        // int cnt = 0;
+        // for(int rp=0; rp<arr.length; rp++){
+
+        //     if(arr[rp] == 0) cnt++;
+        //     while(cnt > k) {
+        //         if(arr[lp] == 0) cnt--;
+        //         lp++;
+        //     }
+
+        //     answer = Math.max(answer, rp - lp + 1);
+        // }
 
         return answer;
     }
