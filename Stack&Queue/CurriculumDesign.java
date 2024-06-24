5e7fb28b63bf43f4162b65ffa7d49f283ff1b715
1s-sky <bivoaov5@gmail.com>
2024-06-24 16:26:00 +0900

    7.교육과정 설계

---
+++ Stack&Queue/CurriculumDesign.java
@@ -0,0 +1,33 @@
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class CurriculumDesign {
+    
+    public static boolean solution(String check, String str){
+        Queue<Character> queue = new LinkedList<>();
+        boolean answer = false;
+
+        for(char c : check.toCharArray()){
+            queue.add(c);
+        }
+
+        for(char c : str.toCharArray()){
+            if(c == queue.peek()) queue.poll();
+            if(queue.isEmpty()) {
+                answer = true;
+                break;
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Scanner kb = new Scanner(System.in);
+        String check = kb.next();
+        String str = kb.next();
+
+        if(solution(check, str)) System.out.println("YES");
+        else System.out.println("NO");
+    }
+}
