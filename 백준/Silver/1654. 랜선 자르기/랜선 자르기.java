f8f535a4d8e7bd9897008e529941659a7777c8ac
이승원 <63232417+1s-sky@users.noreply.github.com>
2024-06-25 22:18:45 +0900

    [Silver II] Title: 랜선 자르기, Time: 192 ms, Memory: 17752 KB -BaekjoonHub

---
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
@@ -0,0 +1,43 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+    static int[] lan;
+    static int k,n;
+    static long answer;
+    
+    private static boolean isValid(long md){
+        int cnt = 0;
+        for(int i=0; i<k; i++){
+            cnt += lan[i]/md;
+        }
+        if(cnt >= n) { answer = md; return true; }
+        else return false;
+    }
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] input = br.readLine().split(" ");
+        k = Integer.parseInt(input[0]);
+        n = Integer.parseInt(input[1]);
+        lan = new int[k];
+        
+        long lt, md, rt;
+        
+        for(int i=0; i<k; i++){
+            lan[i] = Integer.parseInt(br.readLine()); 
+        }
+                
+        lt = 1;        
+        rt = Arrays.stream(lan).max().getAsInt();
+        
+        while(lt<=rt){
+            md = (lt + rt) / 2;
+            
+            if(isValid(md)) lt = md + 1;
+            else rt = md - 1;
+        }
+        
+        System.out.println(answer);
+    }
+}
