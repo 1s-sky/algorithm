2c020004a065cbc700ebff269c73e8e63145e10d
1s-sky <bivoaov5@gmail.com>
2024-10-07 15:57:43 +0900

    4.가장 높은 탑 쌓기

---
+++ Dynamic/TallestTower.java
@@ -0,0 +1,38 @@
+package Dynamic;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class TallestTower {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[][] input = new int[n][3];
+        int[] hight = new int[n];
+
+        for(int i=0; i<n; i++){
+            String[] temp = br.readLine().split(" ");
+            input[i][0] = Integer.parseInt(temp[0]);
+            input[i][1] = Integer.parseInt(temp[1]);
+            input[i][2] = Integer.parseInt(temp[2]);
+        }
+
+        Arrays.sort(input, (o1, o2) -> o2[0] - o1[0]);
+
+        for(int i=0; i<n; i++){
+            int max = 0;
+            for(int j=0; j<i; j++){
+                if (input[j][2] > input[i][2] && input[i][1] + hight[j] > max) {  //무게가 덜나가면, 쌓을 수 있다
+                    max = input[i][1] + hight[j];
+                }
+            }
+            if (max == 0) hight[i] = input[i][1];
+            else hight[i] = max;
+        }
+
+        Arrays.sort(hight);
+        System.out.println(hight[hight.length - 1]);
+    }
+}
