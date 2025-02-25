7bc9a5169f1bc83351995fa57183ab51a5bf6116
이승원 <63232417+1s-sky@users.noreply.github.com>
2025-02-25 09:08:41 +0900

    [Silver IV] Title: 학생별 통화 요금 계산, Time: 296 ms, Memory: 23268 KB -BaekjoonHub

---
+++ 백준/Silver/25329. 학생별 통화 요금 계산/학생별 통화 요금 계산.java
@@ -0,0 +1,50 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        HashMap<String, Integer> stuTime = new HashMap<>();
+        HashMap<String, Integer> stuMoney = new HashMap<>();
+
+        for(int i=0; i<n; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            String[] timeStr = st.nextToken().split(":");
+            int hour = Integer.parseInt(timeStr[0]);
+            int minute = Integer.parseInt(timeStr[1]);
+            int time = hour*60 + minute;
+            String name = st.nextToken();
+
+            stuTime.put(name, stuTime.getOrDefault(name, 0)+time);
+        }
+
+        //요금 계산
+        for(String student : stuTime.keySet()){
+            int totTime = stuTime.get(student);
+            if(totTime > 100) {
+                stuMoney.put(student, 10 + (int)Math.ceil(((double)(totTime - 100))/50)*3);
+            }
+            else stuMoney.put(student, 10);
+        }
+
+        List<Map.Entry<String, Integer>> mapList = new ArrayList<>(stuMoney.entrySet());
+    
+        mapList.sort(new Comparator<>() {
+            @Override
+            public int compare(Map.Entry<String, Integer> o1, Map.Entry<String, Integer> o2){
+                if (o2.getValue().compareTo(o1.getValue()) == 0) {  //같으면 이름으로도 비교
+                    return o1.getKey().compareTo(o2.getKey());
+                }
+                return o2.getValue().compareTo(o1.getValue());
+            }
+        });
+
+        for(Map.Entry<String, Integer> entry : mapList){
+            sb.append(entry.getKey()).append(' ').append(entry.getValue()).append('\n');
+        }
+
+        System.out.println(sb.toString());
+    }
+}
