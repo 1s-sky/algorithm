d226a19e9997cfc7c30862306abe223c45708c4b
이승원 <63232417+1s-sky@users.noreply.github.com>
2025-03-05 10:48:12 +0900

    [Platinum V] Title: 오아시스 재결합, Time: 384 ms, Memory: 62752 KB -BaekjoonHub

--- 백준/Platinum/3015. 오아시스 재결합/오아시스 재결합.java
+++ 백준/Platinum/3015. 오아시스 재결합/오아시스 재결합.java
@@ -15,35 +15,28 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        long[] countArr = new long[n];
         long answer = 0;
 
         ArrayDeque<Person> pStack = new ArrayDeque<>();
-        long count = 0;
         
         for(int i=0; i<n; i++){
             Person nowPerson = new Person(Integer.parseInt(br.readLine()));
-            count = 0;
 
             while (!pStack.isEmpty() && pStack.peek().hight <= nowPerson.hight) {
                 if (pStack.peek().hight == nowPerson.hight) {
                     nowPerson.same = pStack.peek().same + 1;
                 }
-                count += pStack.peek().same;
+                answer += pStack.peek().same;
                 pStack.pop();
             }
 
             if(!pStack.isEmpty()){
-                count++; 
+                answer++; 
             }
 
-            countArr[i] = count;
             pStack.push(nowPerson);
         }
 
-        for(long num : countArr){
-            answer += num;
-        }
         System.out.println(answer);
     }
 }
