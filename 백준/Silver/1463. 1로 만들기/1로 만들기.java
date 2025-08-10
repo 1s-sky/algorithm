558f48e0d525aed701876e20dbf6651f1c98e1c9
이승원 <63232417+1s-sky@users.noreply.github.com>
2025-08-10 19:53:57 +0900

    [Silver III] Title: 1로 만들기, Time: 120 ms, Memory: 18200 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -8,21 +8,11 @@ public class Main {
         int num = Integer.parseInt(br.readLine());
         int[] count = new int[num+1];
 
-        for(int i=2; i<=num; i++){
-            if (i%2==0 && i%3==0) {
-                if(count[i-1] > count[i/2]) {
-                    if (count[i/2] > count[i/3]) count[i] = count[i/3]+1;
-                    else count[i] = count[i/2]+1;
-                }
-                else {
-                    if (count[i-1] > count[i/3]) count[i] = count[i/3]+1;
-                    else count[i] = count[i-1]+1;
-                }
+        for (int i=2; i<=num; i++) {
+                count[i] = count[i - 1] + 1; // 1을 빼는 경우
+                if (i%2 == 0) count[i] = Math.min(count[i], count[i/2] + 1);
+                if (i%3 == 0) count[i] = Math.min(count[i], count[i/3] + 1);
             }
-            else if(i%2==0) count[i] = Math.min(count[i-1], count[i/2]) + 1;
-            else if(i%3==0) count[i] = Math.min(count[i-1], count[i/3]) + 1;
-            else count[i] = count[i-1]+1;
-        }
 
         System.out.println(count[num]);
     }
