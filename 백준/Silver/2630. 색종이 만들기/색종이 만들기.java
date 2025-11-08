a96eda9a6eac8816a8a5ab7e6a9997497b8e159f
이승원 <63232417+1s-sky@users.noreply.github.com>
2025-11-08 21:53:42 +0900

    [Silver II] Title: 색종이 만들기, Time: 136 ms, Memory: 15600 KB -BaekjoonHub

--- 백준/Silver/2630. 색종이 만들기/색종이 만들기.java
+++ 백준/Silver/2630. 색종이 만들기/색종이 만들기.java
@@ -26,13 +26,12 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
+        int n = Integer.parseInt(br.readLine());
         paper = new int[n][n];
         answer = new int[2];
 
         for(int i=0; i<n; i++){
-            st = new StringTokenizer(br.readLine());
+            StringTokenizer st = new StringTokenizer(br.readLine());
             for(int j=0; j<n; j++){
                 paper[i][j] = Integer.parseInt(st.nextToken());
             }
