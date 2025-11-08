c493f061c20bd4a9fd35823e957a85ac78103b67
이승원 <63232417+1s-sky@users.noreply.github.com>
2025-11-08 21:46:45 +0900

    [Silver II] Title: 색종이 만들기, Time: 140 ms, Memory: 15624 KB -BaekjoonHub

---
+++ 백준/Silver/2630. 색종이 만들기/색종이 만들기.java
@@ -0,0 +1,46 @@
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+public class Main {
+    static int[][] paper;
+    static int[] answer;
+
+    static void checkColor(int x, int y, int n) {
+        int color = paper[x][y];
+        for(int i=x; i<x+n; i++){
+            for(int j=y; j<y+n; j++){
+                if (paper[i][j] != color) {
+                    checkColor(x, y, n/2);
+                    checkColor(x, y+n/2, n/2);
+                    checkColor(x+n/2, y, n/2);
+                    checkColor(x+n/2, y+n/2, n/2);
+                    return;
+                }
+            }
+        }
+
+        answer[color]++; 
+        return;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        paper = new int[n][n];
+        answer = new int[2];
+
+        for(int i=0; i<n; i++){
+            st = new StringTokenizer(br.readLine());
+            for(int j=0; j<n; j++){
+                paper[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        checkColor(0,0, n);
+
+        System.out.println(answer[0]);
+        System.out.println(answer[1]);
+    }
+}
